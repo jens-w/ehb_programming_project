c5c6aec7e083111285ca61c8c9b0299fff51824f
ndadde <ndadde@gmail.com>
2020-11-22 15:48:40 +0100

    Initial project creation
    Initial project creation

---
+++ API/coursequiz/src/test/java/com/brielage/coursequiz/CoursequizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brielage.coursequiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursequizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
