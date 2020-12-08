15ec7eb92dea6ee7df7c2f78314879b7ed43e0e9
Jens Wielfaert <jenswielfaert@gmail.com>
2020-12-08 17:56:05 +0100

    User class changed to single imports.
    Changed from wildcard to single imports.

--- API/coursequiz/src/main/java/com/brielage/coursequiz/domain/User.java
+++ API/coursequiz/src/main/java/com/brielage/coursequiz/domain/User.java
@@ -1,6 +1,13 @@
 package com.brielage.coursequiz.domain;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
 @SuppressWarnings("unused")
