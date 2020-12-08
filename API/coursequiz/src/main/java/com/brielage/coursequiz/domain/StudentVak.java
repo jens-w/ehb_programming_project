29da55ecb6974d1ad8e9652c466714d920dc5429
Jens Wielfaert <jenswielfaert@gmail.com>
2020-12-08 18:04:04 +0100

    Updated StudentVak.
    Removed unused import.

--- API/coursequiz/src/main/java/com/brielage/coursequiz/domain/StudentVak.java
+++ API/coursequiz/src/main/java/com/brielage/coursequiz/domain/StudentVak.java
@@ -4,7 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.IdClass;
 import javax.persistence.Table;
-import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 
 @SuppressWarnings("unused")
