7c92c84a5c9bc0ca65e52ad8035a851d6570d4aa
steffen-dc <steffen.de.coster@student.ehb.be>
2021-01-09 13:53:20 +0100

    User can answer questions

--- Discordbot/src/main/java/ehb/programmingproject/coursequiz/Reactions/Emojis.java
+++ Discordbot/src/main/java/ehb/programmingproject/coursequiz/Reactions/Emojis.java
@@ -3,19 +3,21 @@ package ehb.programmingproject.coursequiz.Reactions;
 import java.util.HashMap;
 
 public class Emojis {
-    public static HashMap<Integer, String> map = new HashMap<Integer, String>();
+    public static HashMap<Integer, String> abcs = new HashMap<Integer, String>();
+    public static HashMap<String, String> signs = new HashMap<String, String>();
 
     static{
-        map.put(0, "\uD83C\uDDE6");
-        map.put(1, "\uD83C\uDDE7");
-        map.put(2, "\uD83C\uDDE8");
-        map.put(3, "\uD83C\uDDE9");
-        map.put(4, "\uD83C\uDDEA");
-        map.put(5, "\uD83C\uDDEB");
-        map.put(6, "\uD83C\uDDEC");
-//        map.put("H", "\uD83C\uDDED");
-//        map.put("I", "\uD83C\uDDEE");
-//        map.put("arrow", "➡");
-//        map.put("stop", "⛔");
+        abcs.put(0, "\uD83C\uDDE6");
+        abcs.put(1, "\uD83C\uDDE7");
+        abcs.put(2, "\uD83C\uDDE8");
+        abcs.put(3, "\uD83C\uDDE9");
+        abcs.put(4, "\uD83C\uDDEA");
+        abcs.put(5, "\uD83C\uDDEB");
+        abcs.put(6, "\uD83C\uDDEC");
+        abcs.put(7, "\uD83C\uDDED");
+        abcs.put(8, "\uD83C\uDDEE");
+
+        signs.put("arrow", "➡");
+        signs.put("stop", "⛔");
     }
 }
--- Discordbot/src/main/java/ehb/programmingproject/coursequiz/Reactions/Reaction.java
+++ Discordbot/src/main/java/ehb/programmingproject/coursequiz/Reactions/Reaction.java
@@ -1,11 +1,22 @@
 package ehb.programmingproject.coursequiz.Reactions;
 
 import discord4j.core.event.domain.message.ReactionAddEvent;
+import ehb.programmingproject.coursequiz.Commands.Vraag;
 import ehb.programmingproject.coursequiz.Models.User;
 
 public class Reaction {
-    public static void Read(ReactionAddEvent event, User user){
+    public static void Read(ReactionAddEvent event, User user) {
         String reaction = event.getEmoji().asUnicodeEmoji().get().getRaw();
-        String title = event.getMessage().block().getEmbeds().get(0).getTitle().get();
+        String author = event.getMessage().block().getEmbeds().get(0).getAuthor().get().getName().get();
+        String[] authorSplit = author.split(" ");
+
+        if (user.isRegistered()) {
+
+            if (authorSplit[1].equals("vraag")) {
+                Vraag.GradeQuestion(event, reaction, authorSplit[0], authorSplit[2], user);
+            }
+
+    //        if (APIReader.GetVakken(user.getUserkey()).contains(authorSplit[0])){}
+        }
     }
 }
